5bd02fc31e45074e7cf98eaa2c26c99621cbd3c9
epicwax <quentinhusler@gmail.com>
2016-02-03 12:09:33 +0000

    b

--- include/fillit.h
+++ include/fillit.h
@@ -25,6 +25,16 @@ typedef struct		s_check_t
 	int				nb_dot;
 }					t_check_t;
 
+typedef	struct		s_tetris
+{
+	int				i;
+	int				pos1;
+	int				pos2;
+	int				pos3;
+	int				pos4;
+	char			piece[21];
+}					t_tetris;
+
 int				ft_pre_test(char *map);
 int					ft_check_tetriminos(char *map);
 int					*ft_place_tetriminos(char *map);
