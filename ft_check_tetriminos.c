065ab99e290ff39cb182e7d78200e16c0e4a051d
epicwax <quentinhusler@gmail.com>
2016-02-05 11:16:25 +0000

    .

--- ft_check_tetriminos.c
+++ ft_check_tetriminos.c
@@ -45,13 +45,22 @@ static int		ft_test_tetristack(char *map, int i)
 	if (map[i - 1] != '#' && map[i + 1] != '#'
 			&& map[i - 5] != '#' && map[i + 5] != '#')
 		return (0);
+	if ((map[i - 1] == '#' && map[i - 5] == '#') || 
+			(map[i - 1] == '#' && map[i + 1] == '#') ||
+			(map[i - 1] == '#' && map[i + 5] == '#') ||
+			(map[i - 5] == '#' && map[i + 1] == '#') ||
+			(map[i - 5] == '#' && map[i + 5] == '#') ||
+			(map[i + 1] == '#' && map[i + 5] == '#'))
+		return (2);
 	return (1);
 }
 
 int				ft_check_tetriminos(char *map)
 {
 	t_check_t	i;
+	int		nb_x2;
 
+	nb_x2 = 0;
 	S_VAR_INIT;
 	while (map && map[++i.i])
 	{
@@ -59,18 +68,25 @@ int				ft_check_tetriminos(char *map)
 			return (0);
 		if (map[i.i] == '.')
 			i.nb_dot++;
-		if (map[i.i] == '#' && ft_test_tetristack(map, i.i) == 1)
+		if (map[i.i] == '#')
+		{
+			if (ft_test_tetristack(map, i.i) == 2)
+				nb_x2++;
+			if (ft_test_tetristack(map, i.i) == 0)
+				return (0);
 			i.nb_dieze++;
+		}
 		if (i.nb_dieze > 12 || i.nb_dieze > 4)
 			return (0);
 		if (map[i.i] == '\n' && (map[i.i + 1] == '\n' || map[i.i + 1] == '\0'))
 		{
 			if (i.nb_dot != 12 || i.nb_dieze != 4 || map[i.i - 1] == '\n' || 
-				i.nb_tetriminos > 26)
+				i.nb_tetriminos > 26 || nb_x2 < 2)
 				return (0);
 			i.nb_dieze = 0;
 			i.nb_dot = 0;
 			i.nb_tetriminos++;
+			nb_x2 = 0;
 		}
 	}
 	if (i.nb_tetriminos < 0)
